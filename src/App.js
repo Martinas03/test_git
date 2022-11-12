8dfb07c05eb4db1a1b3e6bc436859966ef35a63d
Martinas03 <martinas1990@mail.ru>
2022-11-13 01:21:32 +0300

    Add list

--- src/App.js
+++ src/App.js
@@ -19,6 +19,11 @@ function App() {
         </a>
       </header>
       <p>Lorem10</p>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
     </div>
   );
 }
